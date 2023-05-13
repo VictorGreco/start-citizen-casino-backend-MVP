d1f8ea30da0eb4579cc3988fe8b4ff55678d2652
Victor Greco <33832977+VictorGreco@users.noreply.github.com>
2023-05-13 11:09:44 +0000

    project init and cors and helmet config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
   imports: [],
   controllers: [AppController],
